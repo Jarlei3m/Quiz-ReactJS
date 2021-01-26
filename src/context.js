7306c72016d730eff8727d0c1db11fae785eec37
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-26 09:57:48 -0300

    chore: Fetch questions

--- src/context.js
+++ src/context.js
@@ -23,6 +23,30 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
+  const fetchQuestions = async(url) => {
+    setLoading(true);
+    setWaiting(false);
+
+    const response = await axios(url).catch(err => console.log(err))
+    
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false)
+      }
+    } else {
+      setWaiting(true);
+      setError(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, [])
+
   return (
     <AppContext.Provider value={
       {
